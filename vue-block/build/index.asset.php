4c108923d92183dcc18f14a68bc250ce1cfcae54
jcodigital <support@jco.fi>
2024-05-03 13:19:27 +0000

    [skip ci] Gutenberg block built by Drone CI

--- vue-block/build/index.asset.php
+++ vue-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '54797f9975c8e2a9f007');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '0c9301abc8bf47ce2b33');
